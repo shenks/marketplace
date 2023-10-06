ef099ea09942d0589bad7a80c3c1bd32aaaf426e
shenkslee <shenkslee@gmail.com>
2023-10-06 18:20:13 +0800

    signup functionality for auth, added /success page

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public", { maxAge: 0 }));
 
+//========================================================================
+//fyi https://www.npmjs.com/package/express-session
 //initialize session, use session package required above
 app.use(
   session({
@@ -30,6 +32,14 @@ app.use(
     saveUninitialized: false,
   })
 );
+//========================================================================
+
+//fyi https://www.npmjs.com/package/passport-local-mongoose
+//initialize passport package
+app.use(passport.initialize());
+//use passport to deal with session
+app.use(passport.session());
+//========================================================================
 
 //mongoose connection
 mongoose.connect(
@@ -43,7 +53,34 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+//========================================================================
+//salts and hashes for you, and saving into mongodb database
+userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+
 const User = new mongoose.model("User", userSchema);
+
+//========================================================================
+//using passport for serialization
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  console.log("Serializing user:", user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  console.log("Deserializing user by ID:", id);
+  User.findById(id)
+    .then(function (user) {
+      console.log("Deserialized user:", user);
+      done(null, user);
+    })
+    .catch(function (err) {
+      console.error("Error deserializing user:", err);
+      done(err, null);
+    });
+});
+//========================================================================
 
 //routes
 app.get("/", function (req, res) {
@@ -58,8 +95,29 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
+app.get("/success", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("success");
+  } else {
+    res.redirect("/login");
+  }
+});
+
 app.post("/signup", function (req, res) {
-  //
+  User.register(
+    { email: req.body.email },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/signup");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/success");
+        });
+      }
+    }
+  );
 });
 
 app.post("/login", function (req, res) {
