938fe6b5fdac8559d9dc50b81c4714290ce3f9fc
shenkslee <shenkslee@gmail.com>
2023-10-06 15:58:55 +0800

    css for form, packages for auth, mongo connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+//importing packages & env config
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//for session management
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+//oauth
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+//
 
 const app = express();
 
@@ -8,6 +21,29 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public", { maxAge: 0 }));
+
+//initialize session, use session package required above
+app.use(
+  session({
+    secret: "marketplace secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//mongoose connection
+mongoose.connect(
+  "mongodb+srv://shenkslee:marketplacepassword@cluster0.kd7k2fx.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+//user schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
 
 //routes
 app.get("/", function (req, res) {
